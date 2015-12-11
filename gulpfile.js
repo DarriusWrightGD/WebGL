d5f6f7fec09a69e273e8283dee418a7bf9586679
Darrius Wright <darrius.wright@extendhealth.com>
2015-12-11 16:51:09 -0700

    Base project set up with one react lint error

--- gulpfile.js
+++ gulpfile.js
@@ -5,48 +5,111 @@ var notify = require('gulp-notify');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-
-
+var connect = require('gulp-connect');
+var streamify = require('gulp-streamify');
+//var browserify = require('gulp-browserify');
+var browserify = require('browserify');
+var babelify = require('babelify')
+var reactify = require('reactify');
+var open = require('gulp-open');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
 
 var src = {
-	scripts:['scripts/**/*.js','scripts/**/*.jsx','!node_modules/**']
+	script:['scripts/**/*.js','scripts/**/*.jsx','!node_modules/**'],
+	style:['css/**/*.css'],
+	html : ['views/**/*.html']
 }
 
+var config = {
+	port : 4565,
+	devBaseUrl : 'http://localhost',
+};
+
+gulp.task('connect', function(){
+	connect.server({
+		root:['dist'],
+		port: config.port,
+		base:  config.devBaseUrl,
+		livereload: true
+	});
+
+});
+
+gulp.task('open', ['connect'], function(){
+	gulp.src('dist/index.html')
+		.pipe(open({url: config.devBaseUrl + ':' + config.port + '/'}))
+});
+
 gulp.task('lint', function(){
-	return gulp.src(src.scripts)
+	return gulp.src(src.script)
+
 	.pipe(eslint({
         extends: 'eslint:recommended',
         ecmaFeatures: {
-            'jsx': true
+        	'es6' : true,
+            'jsx': true,
+            'modules': true
         },
         rules: {
             'strict': 2
         },
         globals: {
             'jQuery':false,
-            '$':true
+            '$':true,
+            'require':false,
+            'modules': false,
+            'module':false,
+            'React':true
         },
         envs: [
             'browser'
+        ],
+        env: { 'es6': true },
+        plugins: [
+        	'react'
         ]
-    }))
+    }))   
 	.pipe(eslint.format())
 	.pipe(notify({message : 'ESlint complete'}));
 });
 
-gulp.task('scripts', function(){
-	return gulp.src(src.scripts)
-		.pipe(babel())
-		.pipe(concat('all.js'))
+gulp.task('html', function(){
+	return gulp.src(src.html)
 		.pipe(gulp.dest('dist'))
+		.pipe(connect.reload())
+		.pipe(notify({message : 'Html bundle complete'}));
+});
+
+gulp.task('styles', function(){
+	return gulp.src(src.style)
+		.pipe('dist/css')
+		.pipe(connect.reload())
+		.pipe(notify({message : 'Style bundle complete'}));
+});
+
+gulp.task('scripts', function(){
+	browserify({
+        debug: true,
+        //extensions: ['es6'],
+        //entries: ['src/test.es6']
+        entries: ['scripts/main.js']
+    	})
+		.transform(babelify)		
+		.bundle()
+		.pipe(source('all.js'))
+		.pipe(gulp.dest('dist/js'))
 		.pipe(rename('all.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
+		// .pipe(streamify(uglify()))
+		.pipe(connect.reload())
+		.pipe(notify({message : 'Javascript bundle complete'}));
 });
 
 gulp.task('watch', function()
 {
-	gulp.watch(src.scripts,['lint','scripts']);
+	gulp.watch(src.script,['lint','scripts']);
+	gulp.watch(src.style, ['styles'])
+	gulp.watch(src.html,['html']);
 });
 
-gulp.task('default', ['lint','scripts', 'watch']);
+gulp.task('default', ['lint','scripts','html','open', 'watch']);
---
+++ scripts/HelloMessage.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+export class HelloMessage extends React.Component{
+    render() {
+      return (
+       <div>Hello {this.props.name}</div>
+      );
+    }
+}
