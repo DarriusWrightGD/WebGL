8a9e7ae45969c83d1ee4511ae2935447a2e837f5
Darrius Wright <darrius.wright@extendhealth.com>
2016-01-13 08:47:15 -0700

    virtual directory works correctly, and fix issues with material ui tabs

--- src/components/EditorTabs.js
+++ src/components/EditorTabs.js
@@ -15,11 +15,10 @@ import 'brace/theme/chaos';
 var {Tab, Tabs} = mui;
 
 function createGuid(){
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
-});
-
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+  });
 }
 
 export default class EditorTabs extends React.Component{
@@ -27,14 +26,15 @@ export default class EditorTabs extends React.Component{
     super(props)
 
     this.onEditorLoad = this.onEditorLoad.bind(this);
+    this.updateTabs = this.updateTabs.bind(this);
     this.tabId = 0;
     this.sharedProps = {
       height: '445px',
       width: '100%',
       theme: 'chaos',
       fontSize: 14,
-      mode: 'text',
-      editorProps:{$blockScrolling: true}
+      mode: 'javascript',
+      editorProps:{$blockScrolling: Infinity}
     }
     this.state = {tabs:[]};
     this.defaultTab = <Tab label='Default Tab' value = {createGuid()}>
@@ -46,7 +46,7 @@ export default class EditorTabs extends React.Component{
         theme={this.sharedProps.theme}
         fontSize={this.sharedProps.fontSize}
         onLoad={this.onEditorLoad}
-        editorProps={this.editorProps}
+        editorProps={{$blockScrolling: true}}
         value={'Select or create a file in the project explorer to get started.'}/>
     </Tab>;
   }
@@ -54,10 +54,8 @@ export default class EditorTabs extends React.Component{
   componentDidMount(){
     this.fileSelectedToken = PubSub.subscribe(Events.fileSelectedEvent, (event, file)=>{
       var guid = createGuid();
-      debugger;
-      if(!_.find(this.state.tabs, function(t){
-        return t.props.key == file.name
-      }))
+
+      if(!_.find(this.state.tabs, function(t){ return t.key == file.name}))
       {
         this.state.tabs.push(
           <Tab
@@ -70,23 +68,31 @@ export default class EditorTabs extends React.Component{
                       return tab;
                     }
                   });
-                this.setState({tabs: filteredTabs, currentTab: filteredTabs[0].props.value})
+                this.updateTabs(filteredTabs);
               }.bind(this,guid)}
               className='glyphicon glyphicon-remove'/>
             </span>
           }
           value={guid}
           key={file.name}
-          onClick = {function(guid){this.setState({currentTab:guid})}.bind(this,guid)}>
+          onClick = {
+            function(guid){
+              var tab = _.find(this.state.tabs, function(t){ return t.props.value == guid});
+              if(tab)
+              {
+                this.setState({currentTab:guid})
+              }
+            }.bind(this,guid)
+          }>
             <AceEditor
             name={file.name}
-            mode={file.mode}
+            mode={this.sharedProps.mode}
             height={this.sharedProps.height}
             width={this.sharedProps.width}
             theme={this.sharedProps.theme}
             fontSize={this.sharedProps.fontSize}
             onLoad={this.onEditorLoad}
-            editorProps={this.editorProps}
+            editorProps={{$blockScrolling: true}}
             value={file.content}/>
           </Tab>
         );
@@ -95,6 +101,12 @@ export default class EditorTabs extends React.Component{
       }
     });
 
+  }
+
+
+  updateTabs(newTabs){
+    this.state.tabs = newTabs;
+    this.setState({tabs: newTabs, currentTab: newTabs[0].props.value})
   }
 
   componentWillUnmount(){
@@ -110,7 +122,7 @@ export default class EditorTabs extends React.Component{
     this.tabContent = this.state.tabs.length > 0 ? this.state.tabs : this.defaultTab;
 
     return (
-      <Tabs onChange={(value)=>{console.log('handle switch : ',value );this.setState({currentTab : value})}} value = {this.state.currentTab} style={{
+      <Tabs value = {this.state.currentTab} style={{
         maxHeight:400,
         marginBottom:20
       }}>
