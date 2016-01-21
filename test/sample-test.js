f8059154762e0280c257eee76db37503de998d74
Darrius Wright <darrius.wright@extendhealth.com>
2016-01-21 09:22:58 -0700

    moving dir

--- test/sample-test.js
+++ test/sample-test.js
@@ -4,7 +4,7 @@ import ReactTestUtils from 'react-addons-test-utils';
 import {createStore} from 'redux';
 import {shaderApp} from '../src/stores/MainStore';
 import Provider from'../src/components/Provider';
-import EditorTab from '~/src/components/EditorTab';
+import EditorTab from 'src/components/EditorTab';
 
 import ShaderAppTheme from '../src/themes/ShaderAppTheme';
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
